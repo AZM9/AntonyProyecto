82705973bbe7c6f9a6d3c67101d015cb3f6b0e9a
AZM9 <by.antonyzm.apk@gmai.com>
2018-06-08 00:44:21 -0500

    Inicio de proyecto

---
+++ WindowsFormsApp1/Conexion.vb
@@ -0,0 +1,47 @@
+﻿Imports Sistem.Data.sqlClient
+
+Public Class Class1
+    Protected cnn As New sqlConnection
+    Public idusuario As Integer
+
+    Protected Function conectado()
+
+        Try
+            cnn = New sqlconnection("data source=(local);initial catalog=dbventas;integrated security=true")
+            cnn.Open()
+            Return True
+
+        Catch ex As Exception
+            msgBox(ex.Message)
+            Return False
+        End Try
+
+
+    End Function
+
+
+    Protected Function desconectado()
+        Try
+            If cnn.Stated = connectionState.Open Then
+                cnn.close()
+                Return True
+            Else
+                Return False
+
+
+            End If
+        Catch ex As Exception
+            msgBox(ex.Message)
+            Return False
+
+
+        End Try
+
+
+
+    End Function
+
+
+
+
+End Class
---
+++ WindowsFormsApp1/fcliente.vb
@@ -0,0 +1,43 @@
+﻿Imports Sistem.Data.sqlClient
+
+Public Class Class1
+    Inherits conexion
+    Dim cmd As New sqlCommand
+
+
+    Public Function mostrar() As datatable
+        Try
+            conectado()
+            cmd = New sqlCommand("mostrar cliente")
+            cmd.commandType = CommandType.StoredProceduce
+            cmd.Connection = cnn
+            If cmd.ExecuteNonQuer Then
+                Dim dt As New DataTable
+                Dim da As New sqlDataAdapter(cmd)
+                da.Fill(dt)
+                Return dt
+
+            Else
+                Return Nothing
+            End If
+
+
+
+
+        Catch ex As Exception
+            msgBox(ex.Messahe)
+            Return Nothing
+        Finally
+            desconectado()
+
+        End Try
+    End Function
+
+
+
+
+
+
+
+
+End Class
